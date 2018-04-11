a1f278427bb2ba647d1a068852cb27bc418a5fae
lessp <ekander.tom@gmail.com>
2018-04-11 14:46:04 +0200

    Removed Axel's boiler-plate

--- src/index.js
+++ src/index.js
@@ -1,35 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { AUTH } from './Redux/Actions/Sessions/Types'
-import reducers from './Redux/Reducers'
-import Routes from './Components/Routes'
-import registerServiceWorker from './registerServiceWorker'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './Assets/Styles/Style.css'
 
-const composeEnhancers = initReduxDevTools()
-const appReducer = combineReducers(reducers)
-const rootReducer = (state, action) => {
-  if (action.type === AUTH)
-    state = undefined
-
-  return appReducer(state, action)
-}
-const middleware = typeof(composeEnhancers) === 'function' ? composeEnhancers(applyMiddleware(thunk)) : applyMiddleware(thunk)
-const store = createStore(rootReducer, middleware)
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
+  <h1>Cobalt</h1>,
   document.getElementById('app')
 )
 
 registerServiceWorker()
-
-function initReduxDevTools() {
-  return process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null
-}
