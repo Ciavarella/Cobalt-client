8f251ff02370167301c0f17470d4b70ddbd2ac77
lessp <ekander.tom@gmail.com>
2018-04-13 12:57:02 +0200

    Add test file for Elements

---
+++ src/Elements/__tests__/Elements.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+
+import Button from "../Button";
+
+it("passed a dummy test", () => expect(2).toBe(2));
