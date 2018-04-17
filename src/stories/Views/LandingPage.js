f064d27260ea7139f769e3dd2517fddf5161b8ea
Unknown <victor.ciavarella@chasacademy.se>
2018-04-17 15:09:36 +0200

    Add landingpage to storybook

---
+++ src/stories/Views/LandingPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+
+import LandingPage from "../../Views/LandingPage";
+
+storiesOf("LandingPage", module).add("LandingPage", () => <LandingPage />);
