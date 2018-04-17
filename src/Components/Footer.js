cb4d9c326856eb4ec8013224d491907ae7c95204
Victor Ciavarella <victor.ciavarella@chasacademy.se>
2018-04-17 13:41:04 +0200

    Fixed footer component to use element components

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { css, withStyles } from "../withStyles";
+import FlexContainer from "../Containers/FlexContainer";
+import List from "../Elements/List";
+import ListItem from "../Elements/ListItem";
 
 const Footer = ({ size = "medium", styles, ...props }) => (
   <footer {...css(styles.footer, styles[size])} {...props}>
-    <ul>
-      <li>
-        <a {...css(styles.link)}>About</a>
-      </li>
-      <li>
-        <a {...css(styles.link)}>Press</a>
-      </li>
-      <li>
-        <a {...css(styles.link)}>Developers</a>
-      </li>
-      <li>
-        <a {...css(styles.link)}>Channels</a>
-      </li>
-    </ul>
+    <FlexContainer direction="column" align="start">
+      <List direction="row" listType="ul">
+        <ListItem>
+          <a href="#">About</a>
+        </ListItem>
+        <ListItem>
+          <a href="#">Press</a>
+        </ListItem>
+        <ListItem>
+          <a href="#">Developers</a>
+        </ListItem>
+        <ListItem>
+          <a href="#">Channels</a>
+        </ListItem>
+      </List>
+    </FlexContainer>
   </footer>
 );
 
@@ -24,10 +29,6 @@ export default withStyles(({ themes, text }) => {
   return {
     footer: {
       padding: "12px"
-    },
-
-    link: {
-      padding: "14px"
     },
 
     /* Color */
