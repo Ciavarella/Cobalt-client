7352843da60c7828052bf4b3cf1932292d1c783d
Andreas Sjölund <andreas.sjolund@chasacademy.se>
2018-04-18 15:27:21 +0200

    move SubscriptionCard story and component to Component dir from Views dir

--- src/stories/Components/SubscriptionCard.js
+++ src/stories/Components/SubscriptionCard.js
@@ -2,6 +2,6 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 
-import SubscriptionCard from "../../Views/SubscriptionCard";
+import SubscriptionCard from "../../Components/SubscriptionCard";
 
 storiesOf("Subscription card", module).add("cards", () => <SubscriptionCard />);
