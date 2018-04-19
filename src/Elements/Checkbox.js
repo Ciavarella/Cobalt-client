0e848209f56b085b70dd207111c30da6e8b35e7f
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-04-19 16:22:31 +0200

    initial refactoring of checkbox component

--- src/Elements/Checkbox.js
+++ src/Elements/Checkbox.js
@@ -2,13 +2,16 @@ import React from "react";
 import { css, withStyles } from "../withStyles";
 
 const Checkbox = ({ styles, ...props }) => (
-  <input {...css(styles.checkbox)} {...props} />
+  <label {...css(styles.container)}>
+    <input {...css(styles.checkbox)} {...props} />
+    <span {...css(styles.checkmark)} />
+  </label>
 );
 
 export default withStyles(({ theme }) => {
   return {
-    checkbox: {
-      margin: "20px"
-    }
+    checkbox: {},
+    container: {},
+    checkmark: {}
   };
 })(Checkbox);
