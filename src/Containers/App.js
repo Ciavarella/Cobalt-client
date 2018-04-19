4bf8c741ecb7fe281a75a68776ec39b97e8205ee
lessp <ekander.tom@gmail.com>
2018-04-19 17:30:49 +0200

    Adds Socket IO for client side and a basic dummy route (with HOC)

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import NotFound from "../Views/NotFound";
+import SocketClient from "../Views/Client";
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <main>
           <Switch>
             <Route exact path="/" />
+            <Route path="/:sessionId" component={SocketClient} />
             <Route path="*" component={NotFound} />
           </Switch>
         </main>
