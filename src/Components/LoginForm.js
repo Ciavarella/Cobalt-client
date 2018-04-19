a70f67bf284415adecb748a97164ee1c7303f444
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-04-19 11:20:06 +0200

    add states for email, password, add handlers

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -10,6 +10,24 @@ import InputWithIcon from "../Elements/InputWithIcon";
 class LoginForm extends React.Component {
   constructor({ styles, ...props }) {
     super(props);
+    this.state = {
+      email: "",
+      password: "",
+      btnDisabled: false //This can be used for disabling button until email/password is a certain length
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.email, this.state.password);
+    //Todo: On submit send both email and password for validation
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
@@ -17,36 +35,48 @@ class LoginForm extends React.Component {
       <div>
         <FlexContainer>
           <Heading size="2">Log in as presenter</Heading>
-          <FlexContainer align="start" style={{ width: "400px" }}>
-            <label style={{ marginBottom: "5px" }} for="email">
-              Email
-            </label>
-            <InputWithIcon
-              name="email"
-              appearance="primary"
-              icon={<i class="fas fa-check" />}
-              iconPosition="right"
-              iconAppearance="primary"
-              type="text"
-              placeholder="Email..."
-            />
-            <label
-              for="password"
-              style={{ marginTop: "20px", marginBottom: "5px" }}
-            >
-              Password
-            </label>
-            <InputWithIcon
-              name="password"
-              appearance="primary"
-              icon={<i class="fas fa-check" />}
-              iconPosition="right"
-              iconAppearance="primary"
-              type="text"
-              placeholder="Password..."
-            />
-          </FlexContainer>
-          <Button>Log In</Button>
+          <form onSubmit={this.handleSubmit}>
+            <FlexContainer>
+              <FlexContainer align="start" style={{ width: "400px" }}>
+                <label style={{ marginBottom: "5px" }} for="email">
+                  Email
+                </label>
+                <InputWithIcon
+                  name="email"
+                  appearance="primary"
+                  icon={<i class="fas fa-check" />}
+                  iconPosition="right"
+                  iconAppearance="primary"
+                  type="text"
+                  placeholder="Email..."
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+                <label
+                  for="password"
+                  style={{ marginTop: "20px", marginBottom: "5px" }}
+                >
+                  Password
+                </label>
+                <InputWithIcon
+                  name="password"
+                  appearance="primary"
+                  icon={<i class="fas fa-check" />}
+                  iconPosition="right"
+                  iconAppearance="primary"
+                  type="text"
+                  placeholder="Password..."
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+              </FlexContainer>
+              {this.state.btnDisabled ? (
+                <Button disabled>Log In</Button>
+              ) : (
+                <Button>Log In</Button>
+              )}
+            </FlexContainer>
+          </form>
           <Paragraph>
             Don't have an account? <a href="#">Sign up here!</a>
           </Paragraph>
