c89e0dd74e2297c098fc2e35c508ce989a53b0a4
joakimunge <ashba@unge.se>
2018-04-19 15:54:18 +0200

    Include open-box on NotFound view

--- src/Views/NotFound.js
+++ src/Views/NotFound.js
@@ -5,9 +5,12 @@ import Heading from "../Elements/Heading";
 import Paragraph from "../Elements/Paragraph";
 import Button from "../Elements/Button";
 
+import openBoxIcon from "../assets/open-box.svg";
+
 const NotFound = ({ url, styles, ...props }) => (
   <div {...css(styles.notFound)}>
     <FlexContainer>
+      <img {...css(styles.icon)} src={openBoxIcon} />
       <Heading size="2">404 page not found</Heading>
       <Paragraph>
         We are sorry but the page: "{url}" could not be found
@@ -25,6 +28,10 @@ export default withStyles(({ colors }) => {
       alignItems: "center",
       height: "100vh",
       backgroundColor: colors.sand
+    },
+    icon: {
+      width: "128px",
+      marginBottom: "48px"
     }
   };
 })(NotFound);
