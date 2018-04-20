34ecb23028e15c97e56aaeaa2f766f21dbd5d7ff
Unknown <victor.ciavarella@chasacademy.se>
2018-04-20 15:27:40 +0200

    Add SessionItem to storybook.

---
+++ src/stories/Components/SessionItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+
+import SessionItem from "../../Components/SessionItem";
+
+storiesOf("Session Item", module).add("Session item", () => <SessionItem />);
