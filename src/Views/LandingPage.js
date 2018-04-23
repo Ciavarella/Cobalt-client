635a17d5c640adfa28643e94111c3c17bf183cb1
Joakim Unge <ashba@unge.se>
2018-04-23 16:12:27 +0200

    Update landingpage component

--- src/Views/LandingPage.js
+++ src/Views/LandingPage.js
@@ -1,19 +1,50 @@
 import React from "react";
 import { css, withStyles } from "../withStyles";
 
+import JoinSession from "../Components/JoinSession";
+
 const LandingPage = ({ styles, ...props }) => {
-  return <div {...css(styles.landingPage)} />;
+  return (
+    <div {...css(styles.content)}>
+      <JoinSession />
+
+      <div {...css(styles.background)} />
+    </div>
+  );
 };
 
 export default withStyles(({ colors, gradients }) => {
   return {
-    landingPage: {
+    background: {
       display: "flex",
       height: "100%",
       width: "100%",
       position: "absolute",
       zIndex: "-99",
-      background: gradients.carbon
+      background:
+        "url('https://images.unsplash.com/photo-1490806230066-f7e6f7bf5052?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2ca889535ea01f912f94ac4ddf0034e0&auto=format&fit=crop&w=2066&q=80')",
+      backgroundSize: "cover",
+      ":before": {
+        content: '""',
+        top: "0",
+        left: "0",
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+        display: "block",
+        zIndex: "1000",
+        background: "rgba(0,0,0,0.5)"
+      }
+    },
+    content: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      position: "absolute",
+      top: "0",
+      left: "0",
+      height: "100%",
+      width: "100%"
     },
     icon: {
       width: "128px",
