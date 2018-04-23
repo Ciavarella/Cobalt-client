781b9a5a7f2b22737b46d0efe2ca91e4a311574c
Unknown <victor.ciavarella@chasacademy.se>
2018-04-23 16:11:25 +0200

    Add SessionItem to storybook

--- src/stories/Components/SessionItem.js
+++ src/stories/Components/SessionItem.js
@@ -4,4 +4,20 @@ import { storiesOf } from "@storybook/react";
 
 import SessionItem from "../../Components/SessionItem";
 
-storiesOf("Session Item", module).add("Session item", () => <SessionItem />);
+storiesOf("Session Item", module)
+  .add("Session item", () => (
+    <SessionItem
+      description="This is a description"
+      title="Awsome title here"
+      date="20 April 2018"
+      attendees="233"
+    />
+  ))
+  .add("Passing description", () => (
+    <SessionItem
+      description="This is a really really long description, not everything should be shown here beacuse it only takes 80 characters"
+      title="This is my title"
+      date="14 March 2017"
+      attendees="133"
+    />
+  ));
