46bdb27c77a42f6c80ccd96103d559d9d3068bc7
joakimunge <ashba@unge.se>
2018-04-24 11:14:39 +0200

    Temp add session cards to My Sessions

--- src/Views/Dashboard/Sessions.js
+++ src/Views/Dashboard/Sessions.js
@@ -4,19 +4,42 @@ import FlexContainer from "../../Containers/FlexContainer";
 import Heading from "../../Elements/Heading";
 import Paragraph from "../../Elements/Paragraph";
 import Button from "../../Elements/Button";
+import SessionItem from "../../Components/SessionItem";
 
 import openBoxIcon from "../../assets/open-box.svg";
 
 const Sessions = ({ styles, ...props }) => {
+  let sessions = ["1"];
+  if (sessions.length < 1) {
+    return (
+      <div {...css(styles.empty)}>
+        <FlexContainer justify="center" align="center">
+          <img {...css(styles.icon)} src={openBoxIcon} alt="Empty Box" />
+          <Heading size="2">You don't have any sessions saved</Heading>
+          <Paragraph>
+            Why don't you get started right now? Let's host your ﬁrst session!
+          </Paragraph>
+          <Button appearance="primaryGradient">Host your first session!</Button>
+        </FlexContainer>
+      </div>
+    );
+  }
+
   return (
     <div {...css(styles.sessions)}>
-      <FlexContainer justify="center" align="center">
-        <img {...css(styles.icon)} src={openBoxIcon} alt="Empty Box" />
-        <Heading size="2">You don't have any sessions saved</Heading>
-        <Paragraph>
-          Why don't you get started right now? Let's host your ﬁrst session!
-        </Paragraph>
-        <Button appearance="primaryGradient">Host your first session!</Button>
+      <FlexContainer
+        align="start"
+        justify="start"
+        direction="row"
+        style={{ flexWrap: "wrap" }}
+      >
+        <SessionItem />
+        <SessionItem />
+        <SessionItem />
+        <SessionItem />
+        <SessionItem />
+        <SessionItem />
+        <SessionItem />
       </FlexContainer>
     </div>
   );
@@ -24,11 +47,17 @@ const Sessions = ({ styles, ...props }) => {
 
 export default withStyles(({ colors }) => {
   return {
-    sessions: {
+    empty: {
       height: "100%",
       display: "flex",
       alignItems: "center",
       justifyContent: "center"
+    },
+    sessions: {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      margin: "0px 12px"
     },
     icon: {
       width: "128px",
