21a132fb5b68e2ffc7c26d58458ce1cd146e21c3
lessp <ekander.tom@gmail.com>
2018-04-25 10:39:27 +0200

    Adds centred utility to storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
+import centered from "@storybook/addon-centered";
 
 import "../index.css";
 
