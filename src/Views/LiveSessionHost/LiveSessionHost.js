59ec85473c1eb7d192fc494e02ca0f4e5ae6b9dd
Joakim Unge <ashba@unge.se>
2018-04-26 22:06:02 +0200

    Clean up in LiveSessionHost

--- src/Views/LiveSessionHost/LiveSessionHost.js
+++ src/Views/LiveSessionHost/LiveSessionHost.js
@@ -8,6 +8,8 @@ import Button from "../../Elements/Button";
 import Heading from "../../Elements/Heading";
 import Paragraph from "../../Elements/Paragraph";
 import Input from "../../Elements/Input";
+import Warning from "./Warning";
+import Engagement from "./Engagement";
 
 /* TODO: Figure out better name */
 class LiveSessionHost extends React.Component {
@@ -16,9 +18,9 @@ class LiveSessionHost extends React.Component {
     this.state = {
       sum: 60,
       audience: 60,
-      threshold: 0.5,
-      red: "24%",
-      green: "76%",
+      threshold: 35,
+      red: "50",
+      green: "50",
       time: "00:00"
     };
 
@@ -83,18 +85,11 @@ class LiveSessionHost extends React.Component {
           </FlexContainer>
         </div>
         <div {...css(styles.graphWrap)}>
-          <div
-            {...css(styles.graphRed)}
-            style={{ width: `${this.state.red}%` }}
-          >
-            <span>{this.state.red > 10 ? `${this.state.red}%` : null}</span>
-          </div>
-          <div
-            {...css(styles.graphGreen)}
-            style={{ width: `${this.state.green}%` }}
-          >
-            <span>{this.state.green > 10 ? `${this.state.green}%` : null}</span>
-          </div>
+          {this.state.red > this.state.threshold ? (
+            <Warning {...this.state} />
+          ) : (
+            <Engagement {...this.state} />
+          )}
         </div>
       </div>
     );
@@ -131,51 +126,6 @@ export default withStyles(({ themes, text, colors }) => {
       flexDirection: "row",
       ":nth-child(1n) span": {
         zIndex: "999"
-      }
-    },
-    graphRed: {
-      height: "100%",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      fontSize: "78px",
-      color: "white",
-      backgroundColor: colors.danger,
-      ":after": {
-        background: "inherit",
-        bottom: "0",
-        content: "''",
-        display: "block",
-        left: "0",
-        position: "absolute",
-        right: "0",
-        transform: "skewX(-4deg)",
-        height: "100%",
-        width: "inherit",
-        transition: "all 0.6s ease"
-      }
-    },
-    graphGreen: {
-      height: "100%",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      fontSize: "78px",
-      color: "white",
-      backgroundColor: colors.success,
-      ":after": {
-        background: "inherit",
-        bottom: "0",
-        content: "''",
-        display: "block",
-        right: "0px",
-        zIndex: "98",
-        position: "absolute",
-        transform: "skewX(-4deg)",
-        transformOrigin: "100%",
-        height: "100%",
-        width: "inherit",
-        transition: "all 0.6s ease"
       }
     },
     timer: {
