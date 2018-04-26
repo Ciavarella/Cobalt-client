c6224ab69307d192321796113e66acede3ecad3a
joakimunge <ashba@unge.se>
2018-04-26 15:09:42 +0200

    Add requireAuth HOC to protect routes

---
+++ src/Components/RequireAuth.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+// import { Loader } from "../";
+
+const requireAuth = ComposedComponent => {
+  class Authentication extends Component {
+    componentWillUpdate(nextProps) {
+      if (!nextProps.isAuthenticated) {
+        return this.props.history.push("/signin");
+      }
+    }
+
+    render() {
+      const { isAuthenticated } = this.props;
+
+      /* When loader component is ready */
+      // if (isFetching) {
+      //   return <Loader />;
+      // }
+
+      if (isAuthenticated) {
+        return <ComposedComponent {...this.props} />;
+      }
+      console.log("You are not authenticated");
+      return <Redirect to="/signin" />;
+    }
+  }
+
+  const mapStateToProps = state => {
+    return {
+      isAuthenticated: state.auth.isAuthenticated
+    };
+  };
+
+  return connect(mapStateToProps)(Authentication);
+};
+
+export default requireAuth;
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+import requireAuth from "../Components/RequireAuth";
 
 import NotFound from "../Views/NotFound";
 import SocketClient from "../Views/Client";
