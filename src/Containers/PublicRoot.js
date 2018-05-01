8a9b633be65abcc2781fba62755694e595603c4c
Unknown <victor.ciavarella@chasacademy.se>
2018-05-01 13:58:37 +0200

    Changed publicroot to work with isAuthenticated

--- src/Containers/PublicRoot.js
+++ src/Containers/PublicRoot.js
@@ -2,12 +2,16 @@ import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
-const withPublicRoot = ComposedComponent => {
-  return class PublicRoot extends React.Component {
+import { connect } from "react-redux";
+
+let withPublicRoot = ComposedComponent => {
+  class PublicRoot extends React.Component {
     render() {
+      const isAuthenticated = this.props.isAuthenticated;
+
       return (
         <React.Fragment>
-          <Header />
+          <Header isAuthenticated={isAuthenticated} />
           <main className="App-content">
             <ComposedComponent />
           </main>
@@ -15,7 +19,13 @@ const withPublicRoot = ComposedComponent => {
         </React.Fragment>
       );
     }
-  };
+  }
+
+  const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+  });
+
+  return connect(mapStateToProps)(PublicRoot);
 };
 
 export default withPublicRoot;
