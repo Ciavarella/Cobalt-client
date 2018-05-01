630351bbba6af803cb506284aa21163abee8f552
Joakim Unge <ashba@unge.se>
2018-05-01 15:50:48 +0200

    Add modal confirmation on cancel

--- src/Views/LiveSessionHost/Lobby.js
+++ src/Views/LiveSessionHost/Lobby.js
@@ -5,30 +5,74 @@ import Heading from "../../Elements/Heading";
 import Paragraph from "../../Elements/Paragraph";
 import Button from "../../Elements/Button";
 import CopyTextfield from "../../Elements/CopyTextfield";
+import Modal from "../../Components/Modal";
 
-const Lobby = ({ styles, ...props }) => {
-  return (
-    <div {...css(styles.lobby)}>
-      <FlexContainer direction="row" fullWidth="1" justify="end">
-        <Button appearance="danger">Cancel session </Button>
-        <Button appearance="secondary">Settings </Button>
-      </FlexContainer>
-      <FlexContainer flex="1" align="center" justify="center">
-        <Heading size="2">Your session is ready!</Heading>
-        <Paragraph>
-          Whenever you are ready, click the button to start!
-        </Paragraph>
-        <Button appearance="primary" onClick={props.startSession}>
-          Let's go!
-        </Button>
-        <Heading size="3">{props.data.attendees} attendees in lobby</Heading>
-      </FlexContainer>
-      <FlexContainer fullWidth="1" align="end" justify="end">
-        <CopyTextfield url={props.sessionId} />
-      </FlexContainer>
-    </div>
-  );
-};
+class Lobby extends React.Component {
+  constructor({ styles, ...props }) {
+    super(...props);
+    this.styles = styles;
+    this.state = {
+      showModal: false
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      showModal: !this.state.showModal
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div {...css(this.styles.lobby)}>
+        {this.state.showModal ? (
+          <Modal withAnimation>
+            <Heading size="3" appearance="white">
+              Are you sure you want to cancel this session?
+            </Heading>
+            <Paragraph appearance="white">
+              You will have to create a new session.
+            </Paragraph>
+            <FlexContainer direction="row">
+              <Button appearance="danger" onClick={this.props.stopSession}>
+                Yes, cancel
+              </Button>
+              <Button appearance="secondary" onClick={this.toggleModal}>
+                No
+              </Button>
+            </FlexContainer>
+          </Modal>
+        ) : (
+          " "
+        )}
+        <FlexContainer direction="row" fullWidth="1" justify="end">
+          <Button appearance="danger" onClick={this.toggleModal}>
+            Cancel session
+          </Button>
+          <Button appearance="secondary">Settings </Button>
+        </FlexContainer>
+        <FlexContainer flex="1" align="center" justify="center">
+          <Heading size="2">Your session is ready!</Heading>
+          <Paragraph>
+            Whenever you are ready, click the button to start!
+          </Paragraph>
+          <Button appearance="primary" onClick={this.props.startSession}>
+            Let's go!
+          </Button>
+          <Heading size="3">
+            {this.props.data.attendees} attendees in lobby
+          </Heading>
+        </FlexContainer>
+        <FlexContainer fullWidth="1" align="end" justify="end">
+          <CopyTextfield url={this.props.sessionId} />
+        </FlexContainer>
+      </div>
+    );
+  }
+}
 
 export default withStyles(({ colors }) => {
   return {
