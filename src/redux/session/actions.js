d46facaffd3bd4d08b7430104ffe8c2ab879e0ac
Robert Jarske <robert.jarske.eriksson@chasacademy.se>
2018-05-01 14:01:17 +0200

    add requestNewSession action

--- src/redux/session/actions.js
+++ src/redux/session/actions.js
@@ -13,3 +13,26 @@ export const requestSessionFail = err => ({
   type: REQUEST_SESSION_FAIL,
   payload: err
 });
+
+export const requestNewSession = data => dispatch => {
+  dispatch(requestSessionStart());
+
+  fetch(`${process.env.REACT_APP_API_BASE_URL}/api/session`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    credentials: "include",
+    body: JSON.stringify(data)
+  })
+    .then(handleResponse)
+    .then(data => dispatch(requestSessionSuccess(data)))
+    .catch(err => dispatch(requestSessionFail(err)));
+};
+
+const handleResponse = response => {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return response.json();
+};
