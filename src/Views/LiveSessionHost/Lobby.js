35289edbe2de6b18f3115045d327a06c195d72c5
Joakim Unge <ashba@unge.se>
2018-05-01 15:05:51 +0200

    Turn Lobby into stateless component

--- src/Views/LiveSessionHost/Lobby.js
+++ src/Views/LiveSessionHost/Lobby.js
@@ -6,50 +6,29 @@ import Paragraph from "../../Elements/Paragraph";
 import Button from "../../Elements/Button";
 import CopyTextfield from "../../Elements/CopyTextfield";
 
-import io from "socket.io-client";
-
-class Lobby extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      attendees: 0
-    };
-    this.socket = io(process.env.REACT_APP_API_BASE_URL);
-  }
-
-  listenForEvents() {
-    this.socket.on("updateHost", data => {
-      console.log(data);
-      this.setState({
-        attendees: data.attendees,
-        settings: data.settings
-      });
-    });
-  }
-
-  render() {
-    const { styles } = this.props;
-    return (
-      <div {...css(styles.lobby)}>
-        <FlexContainer direction="row" fullWidth="1" justify="end">
-          <Button appearance="danger">Cancel session </Button>
-          <Button appearance="secondary">Settings </Button>
-        </FlexContainer>
-        <FlexContainer flex="1" align="center" justify="center">
-          <Heading size="2">Your session is ready!</Heading>
-          <Paragraph>
-            Whenever you are ready, click the button to start!
-          </Paragraph>
-          <Button appearance="primary">Let's go!</Button>
-          <Heading size="3">{this.state.attendees} attendees in lobby</Heading>
-        </FlexContainer>
-        <FlexContainer fullWidth="1" align="end" justify="end">
-          <CopyTextfield url="http://feed.io/xby6Jnb" />
-        </FlexContainer>
-      </div>
-    );
-  }
-}
+const Lobby = ({ styles, ...props }) => {
+  return (
+    <div {...css(styles.lobby)}>
+      <FlexContainer direction="row" fullWidth="1" justify="end">
+        <Button appearance="danger">Cancel session </Button>
+        <Button appearance="secondary">Settings </Button>
+      </FlexContainer>
+      <FlexContainer flex="1" align="center" justify="center">
+        <Heading size="2">Your session is ready!</Heading>
+        <Paragraph>
+          Whenever you are ready, click the button to start!
+        </Paragraph>
+        <Button appearance="primary" onClick={props.startSession}>
+          Let's go!
+        </Button>
+        <Heading size="3">{props.data.attendees} attendees in lobby</Heading>
+      </FlexContainer>
+      <FlexContainer fullWidth="1" align="end" justify="end">
+        <CopyTextfield url="http://feed.io/xby6Jnb" />
+      </FlexContainer>
+    </div>
+  );
+};
 
 export default withStyles(({ colors }) => {
   return {
