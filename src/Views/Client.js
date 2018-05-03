9b2431b20d35ed0e5b4b9588ced12a576b5b0a97
Andreas Sjölund <andreas.sjolund@chasacademy.se>
2018-05-03 15:05:56 +0200

    add hard coded data for now

--- src/Views/Client.js
+++ src/Views/Client.js
@@ -5,6 +5,7 @@ import { css, withStyles } from "../withStyles";
 import Button from "../Elements/Button";
 import Heading from "../Elements/Heading";
 import Paragraph from "../Elements/Paragraph";
+import Loader from "../Elements/Loader";
 import FlexContainer from "../Containers/FlexContainer";
 import VoteSlider from "../Components/VoteSlider";
 import Modal from "../Components/Modal";
@@ -15,28 +16,30 @@ const withSocket = WrappedComponent => {
     constructor(props) {
       super(props);
 
-      this.state = {
-        data: {},
-        fireRedirect: false
-      };
       // this.state = {
       //   data: {
-      //     description: {
-      //       title: "Sample title",
-      //       description: "This is a description"
-      //     },
-      //     engagementDescription: {
-      //       up: "Faster",
-      //       down: "Slower"
-      //     },
       //     status: {
-      //       isPaused: false,
-      //       isStopped: false,
-      //       time: "10:40"
+      //       hasStarted: false
       //     }
       //   },
-
+      //   fireRedirect: false
       // };
+      this.state = {
+        data: {
+          presentation: {
+            name: "Example name",
+            description: "Example description"
+          },
+          engagementDescription: {
+            up: "faster",
+            down: "slower"
+          },
+          status: {
+            hasStarted: true
+          }
+        },
+        fireRedirect: false
+      };
 
       const {
         match: {
@@ -64,13 +67,6 @@ const withSocket = WrappedComponent => {
           data: data
         });
       });
-
-      this.socket.on("welcomeMessage", data => {
-        console.log("welcomeMessage", data);
-        this.setState({
-          presentation: data
-        });
-      });
     }
 
     handleVote = value =>
@@ -93,7 +89,7 @@ const withSocket = WrappedComponent => {
         <WrappedComponent
           handleVote={this.handleVote}
           handleClick={this.handleClick}
-          data={this.state}
+          data={this.state.data}
           {...this.props}
         />
       );
@@ -101,63 +97,90 @@ const withSocket = WrappedComponent => {
   };
 };
 
-const Client = ({ data, handleVote, handleClick, styles }) => {
+const Client = ({ data, handleVote, handleClick, styles, ...props }) => {
   console.log(data);
   return (
     <div {...css(styles.client)}>
-      {/* <FlexContainer
-        justify="center"
-        align="center"
-        style={{ height: "100vh" }}
-      >
-        {data.data.status.isStopped ? (
-          <Modal withOverlay>
-            <Heading size="2">The session has ended</Heading>
-            <Paragraph>Thank you for participating</Paragraph>
-            <Button onClick={handleClick} appearance="secondary">
-              LEAVE SESSION
-            </Button>
-          </Modal>
-        ) : (
-          ""
-        )}
-        <FlexContainer>
-          <Heading appearance="primary" size="2">
-            {data.data.description.title}
-          </Heading>
-          <Heading appearance="primary" size="3">
-            {data.data.description.description}
-          </Heading>
-          <Paragraph appearance="white">{data.data.status.time}</Paragraph>
-          {data.data.status.isPaused ? (
+      {data.status.hasStarted ? (
+        <FlexContainer
+          justify="center"
+          align="center"
+          style={{ height: "100vh" }}
+        >
+          {data.status.hasEnded ? (
             <Modal withOverlay>
-              <Heading size="2">This session is currently paused</Heading>
-              <Paragraph>Wait for the host to resume the session</Paragraph>
+              <Heading size="2">The session has ended</Heading>
+              <Paragraph>Thank you for participating</Paragraph>
+              <Button onClick={handleClick} appearance="secondary">
+                LEAVE SESSION
+              </Button>
             </Modal>
           ) : (
             ""
           )}
-          <FlexContainer align="start">
-            <Paragraph appearance="success">
-              {data.data.engagementDescription.up}
-            </Paragraph>
-            <VoteSlider handleVote={handleVote} />
-            <Paragraph appearance="danger">
-              {data.data.engagementDescription.down}
-            </Paragraph>
+          <FlexContainer>
+            <Heading appearance="primary" size="2">
+              {data.presentation.name}
+            </Heading>
+            <Heading appearance="primary" size="3">
+              {data.presentation.description}
+            </Heading>
+            <Paragraph appearance="white">{data.status.time}</Paragraph>
+            {data.status.isPaused ? (
+              <Modal withOverlay>
+                <Heading size="2">This session is currently paused</Heading>
+                <Paragraph>Wait for the host to resume the session</Paragraph>
+              </Modal>
+            ) : (
+              ""
+            )}
+            <FlexContainer align="start">
+              <Paragraph appearance="success">
+                {data.engagementDescription.up}
+              </Paragraph>
+              <VoteSlider handleVote={handleVote} />
+              <Paragraph
+                appearance="danger"
+                style={{ marginTop: "12px", marginBottom: "0px" }}
+              >
+                {data.engagementDescription.down}
+              </Paragraph>
+            </FlexContainer>
           </FlexContainer>
         </FlexContainer>
-        </FlexContainer> */}
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            flexDirection: "column"
+          }}
+        >
+          <Heading size="2" appearance="white" style={{ textAlign: "center" }}>
+            Waiting for host to start session
+          </Heading>
+          <div {...css(styles.spinner)}>
+            <Loader />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
-const socket = withSocket(Client);
+const ClientWithSocket = withSocket(Client);
 
 export default withStyles(({ themes, colors }) => {
   return {
     client: {
       backgroundColor: colors.nightsky
+    },
+    spinner: {
+      ":nth-child(1n) span": {
+        fontSize: "70px"
+      }
     }
   };
-})(socket);
+})(ClientWithSocket);
