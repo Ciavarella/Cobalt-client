ef8bc75bd2f4a0726d40e598f6d9b9c0e4fb94f0
Joakim Unge <ashba@unge.se>
2018-05-07 15:58:49 +0200

    Render sessions and provide options on click

--- src/Views/Dashboard/Sessions.js
+++ src/Views/Dashboard/Sessions.js
@@ -4,57 +4,115 @@ import FlexContainer from "../../Containers/FlexContainer";
 import Heading from "../../Elements/Heading";
 import Paragraph from "../../Elements/Paragraph";
 import Button from "../../Elements/Button";
+import ButtonLink from "../../Elements/ButtonLink";
 import Loader from "../../Elements/Loader";
+import Modal from "../../Components/Modal";
 import SessionItem from "../../Components/SessionItem";
 import openBoxIcon from "../../assets/open-box.svg";
 
-const renderSessions = data => {
-  return data.map(workspace => {
-    return workspace.presentations.map((session, key) => {
-      return (
-        <SessionItem key={key} data={session} workspace={workspace.name} />
-      );
-    });
-  });
-};
+class Sessions extends React.Component {
+  constructor({ styles, ...props }) {
+    super(props);
+    this.state = {
+      showModal: false,
+      sessionId: null
+    };
 
-const Sessions = ({ styles, ...props }) => {
-  if (props.data.isFetching) {
-    return (
-      <div {...css(styles.centered)}>
-        <Loader fillColor="dawn" size="large" />
-      </div>
-    );
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
-  if (!props.data.user.workspaces) {
+  toggleModal(session) {
+    this.setState({
+      showModal: !this.state.showModal,
+      session: session
+    });
+  }
+
+  renderSessions(data) {
+    return data.map(workspace => {
+      return workspace.presentations.map((session, key) => {
+        if (session.hasEnded) {
+          return (
+            <SessionItem
+              toggleModal={() => {
+                this.toggleModal(session);
+              }}
+              key={key}
+              data={session}
+              workspace={workspace.name}
+            />
+          );
+        }
+      });
+    });
+  }
+
+  render() {
+    const { styles } = this.props;
+
+    if (this.props.data.isFetching) {
+      return (
+        <div {...css(styles.centered)}>
+          <Loader fillColor="dawn" size="large" />
+        </div>
+      );
+    }
+
+    if (!this.props.data.user.workspaces) {
+      return (
+        <div {...css(styles.centered)}>
+          <FlexContainer justify="center" align="center">
+            <img {...css(styles.icon)} src={openBoxIcon} alt="Empty Box" />
+            <Heading size="2">You don't have any sessions saved</Heading>
+            <Paragraph>
+              Why don't you get started right now? Let's host your ﬁrst session!
+            </Paragraph>
+            <ButtonLink to={`/dashboard/new`} appearance="primaryGradient">
+              Host your first session!
+            </ButtonLink>
+          </FlexContainer>
+        </div>
+      );
+    }
+
     return (
-      <div {...css(styles.centered)}>
-        <FlexContainer justify="center" align="center">
-          <img {...css(styles.icon)} src={openBoxIcon} alt="Empty Box" />
-          <Heading size="2">You don't have any sessions saved</Heading>
-          <Paragraph>
-            Why don't you get started right now? Let's host your ﬁrst session!
-          </Paragraph>
-          <Button appearance="primaryGradient">Host your first session!</Button>
+      <div {...css(styles.sessions)}>
+        {this.state.showModal ? (
+          <Modal withAnimation withOverlay>
+            <Heading size="3" appearance="white">
+              {this.state.session.name}
+            </Heading>
+            <Paragraph appearance="white">What do you want to do?</Paragraph>
+            <FlexContainer direction="row">
+              <ButtonLink
+                to={`/host/${this.state.session.sessionId}`}
+                appearance="success"
+                onClick={this.toggleModal}
+              >
+                Go to lobby
+              </ButtonLink>
+              <Button appearance="danger" onClick={this.toggleModal}>
+                Delete
+              </Button>
+            </FlexContainer>
+          </Modal>
+        ) : (
+          " "
+        )}
+        <Heading size="3">In progress</Heading>
+
+        <FlexContainer
+          align="start"
+          justify="start"
+          direction="row"
+          style={{ flexWrap: "wrap" }}
+        >
+          {this.renderSessions(this.props.data.user.workspaces)}
         </FlexContainer>
       </div>
     );
   }
-
-  return (
-    <div {...css(styles.sessions)}>
-      <FlexContainer
-        align="start"
-        justify="start"
-        direction="row"
-        style={{ flexWrap: "wrap" }}
-      >
-        {renderSessions(props.data.user.workspaces)}
-      </FlexContainer>
-    </div>
-  );
-};
+}
 
 export default withStyles(({ colors }) => {
   return {
@@ -69,6 +127,7 @@ export default withStyles(({ colors }) => {
       height: "100%",
       display: "flex",
       flexDirection: "column",
+      padding: "12px 0px",
       margin: "0px 12px"
     },
     icon: {
