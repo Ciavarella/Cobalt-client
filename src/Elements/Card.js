d1b42da06406893883596fdc86e365ee059dbfd0
Joakim Unge <ashba@unge.se>
2018-05-07 15:57:52 +0200

    Improve card component

--- src/Elements/Card.js
+++ src/Elements/Card.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { css, withStyles } from "../withStyles";
 
-const Card = ({ appearance = "carbon", shadow = null, styles, ...props }) => (
+const Card = ({
+  appearance = "carbon",
+  shadow = "shadow",
+  styles,
+  ...props
+}) => (
   <div
     {...css(styles.card, styles[appearance], styles[shadow], styles.rounded)}
     {...props}
@@ -12,7 +17,8 @@ export default withStyles(({ themes, shadow, rounded }) => {
   return {
     card: {
       padding: "40px",
-      color: "white"
+      color: "white",
+      position: "relative"
     },
 
     /* Color */
@@ -22,6 +28,9 @@ export default withStyles(({ themes, shadow, rounded }) => {
     success: themes.success,
     danger: themes.danger,
     carbon: themes.carbon,
+    white: {
+      backgroundColor: "white"
+    },
 
     /* Shadow */
     shadow: shadow,
